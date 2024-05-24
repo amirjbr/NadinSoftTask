559c6a958950138dd2d6f316045348447540b940
amirjbr <153921915+amirjbr@users.noreply.github.com>
2024-05-24 21:26:38 +0330

    fixing api response error message

--- NadinSoftTask.Domain/Entities/APIResponse.cs
+++ NadinSoftTask.Domain/Entities/APIResponse.cs
@@ -9,6 +9,10 @@ namespace NadinSoftTask.Domain.Entities
 {
     public class APIResponse
     {
+        public APIResponse()
+        {
+            ErrorMessages = new List<string>();
+        }
         public HttpStatusCode StatusCode { get; set; }
         public bool IsSuccess { get; set; }
         public List<string> ErrorMessages { get; set; }
