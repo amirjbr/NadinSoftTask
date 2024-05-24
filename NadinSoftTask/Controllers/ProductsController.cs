876c1a49f82e4979b40aa7dbc1072ad5522e6baa
amirjbr <153921915+amirjbr@users.noreply.github.com>
2024-05-24 19:31:18 +0330

    creating APIResponse file

--- NadinSoftTask/Controllers/ProductsController.cs
+++ NadinSoftTask/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using NadinSoftTask.Domain.Entities;
 using NadinSoftTask.Domain.Entities.DTO;
 using NadinSoftTask.Domain.Repository;
 using NadinSoftTask.Infrastructure.Data;
+using System.Net;
 
 namespace NadinSoftTask.Controllers
 {
@@ -14,21 +15,28 @@ namespace NadinSoftTask.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly IProductRepository _dbProduct;
-        //private readonly ApplicationDbContext _db;
+        protected APIResponse _response;
         private readonly IMapper _mapper;
         public ProductsController(IProductRepository dbProduct, IMapper mapper)
         {
             _dbProduct = dbProduct;
             _mapper = mapper;
+            this._response = new();
         }
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetProducts()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<APIResponse>> GetProducts()
         {
             IEnumerable<Product> productList = await _dbProduct.GetAllAsync();
-            return Ok(_mapper.Map<List<ProductDTO>>(productList));
+            _response.Result = _mapper.Map<List<ProductDTO>>(productList);
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
         }
         [HttpGet("id", Name = "GetProduct")]
-        public async Task<ActionResult<ProductDTO>> GetProduct(int id)
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<APIResponse>> GetProduct(int id)
         {
             if (id == 0)
             {
@@ -40,11 +48,15 @@ namespace NadinSoftTask.Controllers
                 return NotFound();
             }
 
-            return Ok(_mapper.Map<ProductDTO>(product));
+            _response.Result = _mapper.Map<ProductDTO>(product);
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
         }
 
         [HttpPost]
-        public async Task<ActionResult<ProductDTO>> CreateProduct([FromBody] ProductCreateDTO productCreateDTO)
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<ActionResult<APIResponse>> CreateProduct([FromBody] ProductCreateDTO productCreateDTO)
         {
             if (await _dbProduct.GetByIdAsync(u => u.Name.ToLower() == productCreateDTO.Name.ToLower()) != null)
             {
@@ -56,14 +68,19 @@ namespace NadinSoftTask.Controllers
                 return BadRequest();
             }
 
-            Product model = _mapper.Map<Product>(productCreateDTO);
+            Product product = _mapper.Map<Product>(productCreateDTO);
 
-            await _dbProduct.CreateAsync(model);
-            return CreatedAtRoute("GetProduct", new { id = model.Id }, model);
+            await _dbProduct.CreateAsync(product);
+            _response.Result = _mapper.Map<ProductDTO>(product);
+            _response.StatusCode = HttpStatusCode.Created;
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, _response);
         }
 
         [HttpDelete("id", Name = "ProductDelete")]
-        public async Task<IActionResult> DeleteProduct(int id)
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult<APIResponse>> DeleteProduct(int id)
         {
             if (id == 0)
             {
@@ -78,23 +95,32 @@ namespace NadinSoftTask.Controllers
             }
 
             await _dbProduct.RemoveAsync(product);
-            return NoContent();
+            _response.StatusCode = HttpStatusCode.NoContent;
+            _response.IsSuccess = true;
+            return Ok(_response);
         }
 
         [HttpPut("id", Name = "UpdateProduct")]
-
-        public async Task<IActionResult> UpdateProduct(int id, [FromBody] ProductUpdateDTO productUpdateDto)
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult<APIResponse>> UpdateProduct(int id, [FromBody] ProductUpdateDTO productUpdateDto)
         {
             if (productUpdateDto == null || productUpdateDto.Id != id)
             {
                 return BadRequest();
             }
             Product model = _mapper.Map<Product>(productUpdateDto);
+
             await _dbProduct.UpdateAsync(model);
-            return NoContent();
+
+            _response.StatusCode = HttpStatusCode.NoContent;
+            _response.IsSuccess = true;
+            return Ok(_response);
         }
 
         [HttpPatch("{id:int}", Name = "UpdatePartialProduct")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> UpdatePartialProduct(int id, JsonPatchDocument<ProductUpdateDTO> patchDTO)
         {
             if (patchDTO == null || id == 0)
